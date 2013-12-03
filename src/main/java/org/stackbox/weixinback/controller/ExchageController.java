549723996cd7b4d4dba470ef7256a2d4de72b3bc
SageLyu <superalsrk@gmail.com>
2013-12-03 14:36:20 +0800

    add Request jaxb message

--- src/main/java/org/stackbox/weixinback/controller/ExchageController.java
+++ src/main/java/org/stackbox/weixinback/controller/ExchageController.java
@@ -3,9 +3,11 @@ package org.stackbox.weixinback.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.stackbox.weixinback.message.request.RequestTextMessage;
 import org.stackbox.weixinback.message.response.ResponseTextMessage;
 import org.stackbox.weixinback.util.SignHelper;
 
@@ -29,13 +31,13 @@ public class ExchageController {
 		}
 	}
 	
-	@RequestMapping(value={"/cc"}, method=RequestMethod.GET)
-	public @ResponseBody ResponseTextMessage fetchXML() {
+	@RequestMapping(value={"/corecontroller"}, method=RequestMethod.POST, headers="Accept=application/xml")
+	public @ResponseBody ResponseTextMessage fetchXML(@RequestBody RequestTextMessage reqMsg) {
 		ResponseTextMessage msg = new ResponseTextMessage();
-		msg.setContent("nihao");
-		msg.setCreateTime((new java.util.Date()).getTime());
-		msg.setFormUserName("tls");
-		msg.setToUserName("touser");
+		msg.setContent("喵呜，系统娘正在全力研发中。。。");
+		msg.setCreateTime((new java.util.Date()).getTime()/1000);
+		msg.setFromUserName(reqMsg.getToUserName());
+		msg.setToUserName(reqMsg.getFromUserName());
 		msg.setMsgType("text");
 		return msg;
 	}
--- src/main/java/org/stackbox/weixinback/message/request/RequestTextMessage.java
+++ src/main/java/org/stackbox/weixinback/message/request/RequestTextMessage.java
@@ -1,4 +1,4 @@
-package org.stackbox.weixinback.message.response;
+package org.stackbox.weixinback.message.request;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -7,23 +7,26 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.stackbox.weixinback.common.AdapterXmlCDATA;
 
 @XmlRootElement(name = "xml")
-public class ResponseTextMessage {
+public class RequestTextMessage {
 	private String toUserName;
 	private String fromUserName;
 	private Long createTime;
 	private String msgType;
 	private String content;
+	private Long msgId;
 	
-	public ResponseTextMessage() {}
+	
+	public RequestTextMessage() {}
 
-	public ResponseTextMessage(String toUserName, String fromUserName,
-			Long createTime, String msgType, String content) {
+	public RequestTextMessage(String toUserName, String fromUserName,
+			Long createTime, String msgType, String content, Long msgId) {
 		super();
 		this.toUserName = toUserName;
 		this.fromUserName = fromUserName;
 		this.createTime = createTime;
 		this.msgType = msgType;
 		this.content = content;
+		this.msgId = msgId;
 	}
 
 	@XmlJavaTypeAdapter(AdapterXmlCDATA.class)
@@ -74,5 +77,17 @@ public class ResponseTextMessage {
 	public void setContent(String content) {
 		this.content = content;
 	}
+
+	@XmlJavaTypeAdapter(AdapterXmlCDATA.class)
+	public Long getMsgId() {
+		return msgId;
+	}
+
+	@XmlElement(name="MsgId")
+	public void setMsgId(Long msgId) {
+		this.msgId = msgId;
+	}
+	
+	
 	
 }
